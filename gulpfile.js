048e388bd9f52edc02ba724940a0a1847f65199c
Jordan Rancie <jordan@thetradingbox.com>
2017-05-24 18:57:44 +0700

    New Shop template

--- gulpfile.js
+++ gulpfile.js
@@ -1,155 +1,73 @@
-const gulp = require("gulp");
-const plumberNotifier = require("gulp-plumber-notifier");
-const browserSync = require("browser-sync").create();
-const autoPrefixer = require("gulp-autoprefixer");
-const sass = require("gulp-sass");
-const csscomb = require("gulp-csscomb");
-const rename = require('gulp-rename');
-const csso = require("gulp-csso");
-const inject = require("gulp-inject");
-const series = require("stream-series");
-const uglify = require("gulp-uglify");
-const prettify = require("gulp-html-prettify");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var plumberNotifier = require('gulp-plumber-notifier')
+var clean = require('gulp-clean');
+var pkg = require('./package.json');
 
-// Directories path
-var baseDir = "./static/sass/base/";
-var layoutDir = "./static/sass/layout/";
-var pagesDir = "./static/sass/pages/";
-var themesDir = "./static/sass/themes/";
-var shrotcodesDir = "./static/sass/shortcodes/";
-var styleCss = [baseDir+"*.scss", layoutDir+"*.scss", pagesDir+"*.scss"];
-
-// Autoprefixer config
-const AUTOPREFIXER_BROWSERS = [
-    "last 2 version",
-    "> 1%",
-    "ie >= 9",
-    "ie_mob >= 10",
-    "ff >= 30",
-    "chrome >= 34",
-    "safari >= 7",
-    "opera >= 23",
-    "ios >= 7",
-    "android >= 4",
-    "bb >= 10"
-];
-
-// Server task
-gulp.task("serve", function() {
-    browserSync.init({
-        server: "./",
-        notify: false
-    });
+ 
+gulp.task('clean', function () {
+    return gulp.src('./_site/static/**', {read: false})
+        .pipe(clean());
 });
 
-// Shortcode sass to css
-gulp.task("shortcode", function() {
-    return gulp.src(shrotcodesDir + "shortcodes.scss")
-        .pipe(plumberNotifier())
-        .pipe(sass())
-        .pipe(autoPrefixer(AUTOPREFIXER_BROWSERS))
-        .pipe(csscomb())
-        .pipe(gulp.dest("./static/css"))
-        .pipe(browserSync.stream())
-        .pipe(csso())
-        .pipe(rename({suffix:".min"}))
-        .pipe(gulp.dest("./static/css"));
+gulp.task('copy-static', function() {
+    return gulp.src(['./_static/**']).pipe(gulp.dest('./_site/static'));
+})
+
+gulp.task('copy-images', function() {
+    return gulp.src(['./_images/**']).pipe(gulp.dest('./_site/images'));
+})
+
+gulp.task('copy-js', function() {
+    return gulp.src(['./_static/js/*.js'])
+        .pipe(gulp.dest('./_site/static/js'));
+})
+
+// SASS to css
+gulp.task('sass', function(){
+    return gulp.src([
+            './_sass/style.scss',
+            './_sass/shortcodes/shortcodes.scss',
+            './_sass/default-theme.scss',
+            './_sass/red-theme.scss',
+            './_sass/green-theme.scss'])
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest("./_site/static/css"));
 });
 
-// Theme sass to css
-gulp.task("theme", function() {
-    return gulp.src(themesDir + "*.scss")
-        .pipe(plumberNotifier())
-        .pipe(sass())
-        .pipe(autoPrefixer(AUTOPREFIXER_BROWSERS))
-        .pipe(csscomb())
-        .pipe(gulp.dest("./static/css"))
-        .pipe(browserSync.stream())
-        .pipe(csso())
-        .pipe(rename({suffix:".min"}))
-        .pipe(gulp.dest("./static/css"));
-});
-
-// Style sass to css
-gulp.task("style", function() {
-    return gulp.src("./static/sass/style.scss")
-        .pipe(plumberNotifier())
-        .pipe(sass())
-        .pipe(autoPrefixer(AUTOPREFIXER_BROWSERS))
-        .pipe(csscomb())
-        .pipe(gulp.dest("./static/css"))
-        .pipe(browserSync.stream())
-        .pipe(csso())
-        .pipe(rename({suffix:".min"}))
-        .pipe(gulp.dest("./static/css"));
-});
-
-gulp.task("watch", ["serve"], function() {
-    gulp.watch(shrotcodesDir + "*.scss", ["shortcode"]);
-    gulp.watch(themesDir + "*.scss", ["theme"]);
-    gulp.watch(styleCss, ["style"]);
-
-    gulp.watch("./*.html", browserSync.reload);
-    gulp.watch("./static/js/*.js", browserSync.reload);
-});
-
-gulp.task("inject", function() {
-    var vendorPaths = [
-        "!./static/vendor/jquery",
-        "!./static/vendor/slider-revolution/**",
-        "!./static/vendor/modernizr",
-        "!./static/vendor/backward",
-        "!./static/vendor/bootstrap/**",
-        "!./static/vendor/masonry",
-        "./static/vendor/**",
-        "./static/vendor/**/**",
-        "./static/vendor/**/**/**"
-    ];
-
-    var main = gulp.src([
-        "./static/vendor/modernizr/*.js",
-        "./static/vendor/jquery/**.js",
-        "./static/vendor/bootstrap/**/bootstrap.min.js",
-        "./static/vendor/bootstrap/**/bootstrap.min.css"
-    ], {
-        read: false
-    });
-
-    var project = gulp.src([
-        "./static/js/scripts.js",
-        "./static/css/shortcodes.css",
-        "./static/css/style.css",
-        "./static/css/default-theme.css"
-    ], {
-        read: false
-    });
-
-    var vendorJS = vendorPaths.map(function(item) { return item + "/*.js"; });
-    var vendorCSS = vendorPaths.map(function(item) { return item + "/*.css"; });
-    var libs = gulp.src(vendorJS.concat(vendorCSS), {read:false});
-
-    return gulp.src("./*.html")
-        .pipe(inject(series(main, libs, project), {relative: true}))
-        .pipe(gulp.dest("./"));
-
-    // return gulp.src("./200-resource-paths.html")
-    //     .pipe(inject(series(main, libs, project), {relative: true}))
-    //     .pipe(gulp.dest("./"));
-});
-
-gulp.task("script", function() {
-    return gulp.src("./static/js/scripts.js")
+gulp.task("minify-js", function() {
+     return gulp.src("./_static/js/scripts.js")
         .pipe(plumberNotifier())
         .pipe(uglify())
         .pipe(rename({suffix:".min"}))
-        .pipe(gulp.dest("./static/js/"));
+        .pipe(gulp.dest("./_site/static/js/"));
 
 });
 
 gulp.task("html-prettify", function() {
-    gulp.src('./*.html')
-        .pipe(prettify({indent_char: " ", indent_size: 4}))
+    return gulp.src('./*.html')
+        .pipe(prettify({indent_char: " ", indent_size: 2}))
         .pipe(gulp.dest("./"));
 });
 
-gulp.task("default", ["serve", "shortcode", "style", "theme", "watch"]);
+gulp.task("minify-css", ['sass'], function() {
+    return gulp.src('./_site/static/css/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest("./_site/static/css"));
+});
+
+gulp.task('watch:sass', function () {
+  gulp.watch(['./_sass/**/*.scss','./_sass/*.scss'], ['sass']);
+});
+
+gulp.task('watch:js', function () {
+  gulp.watch("./_static/js/scripts.js", ['copy-js','minify-js']);
+});
+
+
+gulp.task('default', ['clean', 'copy-images', 'copy-static', 'minify-js', 'minify-css', 'watch:sass', 'watch:js']);
+gulp.task('setup', ['clean', 'copy-images', 'copy-static', 'minify-js', 'minify-css']);
